1a868b576377270c120367df2b3148d05dea3d47
MaciekFlis <maciek.flis@gmail.com>
2019-01-05 22:49:53 +0100

    dummy plants controller

---
+++ earth/Controllers/PlantsController.fs
@@ -0,0 +1,19 @@
+namespace Api.Controllers
+
+open Microsoft.AspNetCore.Mvc
+
+type PlantStatus = 
+  {
+    Id : int;
+    Humidity : decimal;
+    Temperature : decimal
+  }
+
+[<Route("plants/{id}")>]
+[<ApiController>]
+type PlantsController () =
+  inherit ControllerBase()
+
+  [<HttpGet>]
+  member this.Get(id : int) = 
+    ActionResult<PlantStatus>( {Id = id; Humidity = 10.0m; Temperature = 18.0m} )
