c37b672fa92d91f7cf34d6ee7eb29b8739b9ba91
MaciekFlis <maciek.flis@gmail.com>
2019-01-06 11:20:36 +0100

    remove use STS header

--- earth/Startup.fs
+++ earth/Startup.fs
@@ -21,8 +21,7 @@ type Startup private () =
         if (env.IsDevelopment()) then
             app.UseDeveloperExceptionPage() |> ignore
         else
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts() |> ignore
+            () |> ignore
 
         app.UseMvc() |> ignore
 
