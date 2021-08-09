66d5836061f6c3002cdde486fcd20600606d3297
MaciekFlis <maciek.flis@gmail.com>
2021-08-09 18:02:13 +0200

    committing before format

--- water/src/Main.hs
+++ water/src/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Arduino.Nano
+import qualified Arduino.Library.LCD as LCD
 
 -- digitalOutput pin13 OK
 -- digitalOutput pin12 OK
@@ -16,4 +17,32 @@ import Arduino.Nano
 -- digitalOutput pin2 OK
 -- digitalOutput pin1 no such pin in lib
 main = compileProgram $
-    digitalOutput pin13 =: (every 20000) ~> toggle
+    let inp = digitalRead pin9 
+    in
+      -- digitalOutput pin6 =: inp
+      setupLCD [ bootup ~> mapSMany (const introText)
+                   , timerDelta ~> mapSMany statusText
+                   ]
+
+statusText :: Expression Arduino.Nano.Word -> [Expression LCD.Command]
+statusText delta = concat
+    [ LCD.position 1 0
+    , LCD.text ":-)"
+    ]
+
+introText :: [Expression LCD.Command]
+introText = concat
+    [ LCD.position 0 0
+    , LCD.text "FRP Arduino"
+    ]
+
+setupLCD :: [Stream LCD.Command] -> Action ()
+setupLCD streams = do
+    LCD.output rs d4 d5 d6 d7 enable =: mergeS streams
+    where
+        rs     = digitalOutput pin3
+        d4     = digitalOutput pin5
+        d5     = digitalOutput pin6
+        d6     = digitalOutput pin7
+        d7     = digitalOutput pin8
+        enable = digitalOutput pin4
